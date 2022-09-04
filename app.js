458e758cabc4f508b9befedcd86e87248dcb0826
sufiyan2401 <110626687+sufiyan2401@users.noreply.github.com>
2022-09-04 17:20:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var x = prompt("Enter First Number")
+var y = prompt("Enter Second Number")
+let a = prompt("Enter an Operator Like -,+,*,/")
+if(a == "+"){
+    var z =Number(x)+Number(y);
+    alert(z);
+}
+else if(a == "*"){
+    var z =Number(x)*Number(y);
+    alert(z);
+}
+else if(a == "-"){
+    var z =Number(x)-Number(y);
+    alert(z);
+}
+else if(a == "/"){
+    var z =Number(x)/Number(y);
+    alert(z);
+}
